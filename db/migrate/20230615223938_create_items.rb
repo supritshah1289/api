f29340cdba504107eaa039867fb9789d9b5c606a
Suprit <suprit@Suprits-MacBook-Pro.local>
2023-06-17 14:37:07 -0400

    First commit api

---
+++ db/migrate/20230615223938_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.string :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+i1 = Item.create(title: "The best tailor in edison,NJ", description: "I am the best and provide the best services with great prices", image_url: "https://media.istockphoto.com/id/1161358262/photo/female-fashion-designer-standing-with-hands-on-hip-in-design-studio.jpg?s=612x612&w=0&k=20&c=QWB5USVkt2WFYMjJYi0pBcC43wwb0t3QXeAmVyTp5ug=");
